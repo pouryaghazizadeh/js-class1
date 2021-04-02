ff79e914ca317be017d3e113fac7de3ef31138d0
pourya <pourya.gh095@gmail.com>
2021-04-02 19:21:16 +0430

    p1

---
+++ class1.js
@@ -0,0 +1,23 @@
+// operators
+
+// arithmetic operators
+// we have  + - * % ** /
+let number1 = 5 ;
+let number2 = 6 ;
+let number3 = "6";
+let answer ;
+
+// answer = number1 + number2
+// answer = number1 - number2
+// answer = number1 * number2
+// answer = number1 / number2
+// answer = number1 % number2
+// answer = number1 ** number2
+answer = number1 + number3
+answer = number1 * number3
+answer = number1 - number3
+
+console.log(answer)
+
+
+ 
