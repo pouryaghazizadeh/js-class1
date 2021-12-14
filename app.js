fd5563e97cdc4daa1349ac5793f1a45b558ec4c3
pourya ghazizadeh <pourya.gh095@gmail.com>
2021-12-14 17:21:35 +0100

    comets

---
+++ app.js
@@ -0,0 +1,133 @@
+// what is variable:
+// the variables  Included anything that can changeable
+
+// ***** variable javascript ***** //
+// you can make variable with 3 way
+
+// var   => do not use this
+// let   => with this you can define mutable data
+// const => with this you can define immutable data
+
+//// note: You may see in the some code use var change var to let
+
+//***** make name for variable *****//
+
+// usually in js, we write the camel case like this =>myName
+// also lower case => name
+//   for more information check pdf js1-1
+
+//*****  make a variable *****//
+
+//  // be for define a variable let see some  type of  variable
+// check the type with method typeOf()
+//  // string
+//  // number
+//  // boolean
+//  // undefine
+//  // null
+// et cetera => you will learn in future
+
+// examples
+
+// let variable1 = "hello world!"
+// let variable2 = 12;
+// let variable3 = true;
+// const myName = "pourya"
+
+///// Numbers in js /////
+
+// let number1 = 12;
+// const number2 = 14;
+
+// arithmetic operators
+// Addition:           +
+// Submission:         -
+// multiplication:     *
+//Divide remaining:    %
+//                     **
+//division:            /
+// ++ increment
+// -- decrement
+
+// let num = 2
+// console.log(num);// 2
+// ++num;
+// console.log(num);//3
+
+// console.log(num);// 2
+// num++;
+// console.log(num);//3
+
+// console.log(num);// 2
+// num = num++;
+// console.log(num);//2
+
+// console.log(num);// 2
+// num = ++num;
+// console.log(num);//3
+
+// assignment operators
+//  +=  -=   /=   *=  %= **=
+
+//// method number in  javascript ////
+// this is some method in js if you want more check mdn
+// Math.PI =>   3.14
+// Math.round()
+// Math.ceil()
+// Math.floor()
+// Math.sqrt()
+// Math.abs()
+// Math.min()
+// Math.max()
+// Math.toString()
+// Math.toFixed()
+// Math.toPrecision()
+// Math.Number ()
+// Math.parseInt()
+// Math.parseFloat()
+
+//// string in js  ////
+
+// for define string:  ' '  "  "
+// let firstName = "pashmak"
+// const lastName = "pashmakian"
+
+//// method string in javascript ////
+
+// concat()
+// uppercase
+// lowercase
+// 
+//includes()
+//substring()
+// slice()
+//split()
+// replace()
+// replaceAll()
+
+/// ** index in string ** ///
+//  start with 0
+// access with [] => [number index]
+// .length for check index
+
+// some example
+///////// example1
+// let str1 = "hi"
+// let str2 = " there"
+// const str3 = str1 + str2
+// console.log(str3);
+
+//// boolean in javascript /////
+// value => true false
+// let err  = true
+// const admin = false
+
+//// null and undefine  /////
+
+// const value1 =  null
+// const value2 = undefined
+
+
+
+
+
